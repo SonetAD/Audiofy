4d2b72f0226838b7c8a29f05633b949a6d321321
Sonet Adhikary <sonetadhikary@Sonets-MacBook-Air.local>
2023-04-06 21:16:25 +0600

    Solve the play and pause button issue

--- js/pdfviewer.js
+++ js/pdfviewer.js
@@ -1,19 +1,19 @@
 const pdfViewer = (() => {
-  const modalPlaceholder = document.getElementById('modal-place-holder');
+  const modalPlaceholder = document.getElementById("modal-place-holder");
 
-  const bookPreviewCard = document.querySelectorAll('.book-preview-card');
+  const bookPreviewCard = document.querySelectorAll(".book-preview-card");
   bookPreviewCard.forEach((targetedBook) => {
-    targetedBook.addEventListener('click', (e) => {
-      const filePath = targetedBook.getAttribute('file-path');
-      let randomId = '';
+    targetedBook.addEventListener("click", (e) => {
+      const filePath = targetedBook.getAttribute("file-path");
+      let randomId = "";
 
       for (let i = 0; i < filePath.length; i++) {
         let c = filePath[i];
-        if (c !== '.' && c !== '/') {
+        if (c !== "." && c !== "/") {
           randomId += c;
         }
       }
-      modalPlaceholder.innerHTML = '';
+      modalPlaceholder.innerHTML = "";
 
       const modal = `
 
@@ -38,11 +38,11 @@ const pdfViewer = (() => {
 </div>
       `;
 
-      e.target.setAttribute('data-bs-toggle', 'modal');
-      e.target.setAttribute('data-bs-target', `#${randomId}`);
+      e.target.setAttribute("data-bs-toggle", "modal");
+      e.target.setAttribute("data-bs-target", `#${randomId}`);
       modalPlaceholder.innerHTML = modal;
 
-      const canvas = document.querySelector('#pdf-render');
+      const canvas = document.querySelector("#pdf-render");
 
       const global = new Global(canvas, 1, 1.5);
       // Getting the document
@@ -59,24 +59,28 @@ const pdfViewer = (() => {
           if (!global.pdfText) {
             showNextPage();
           }
-          global.pdfText = '';
+          global.pdfText = "";
         }
       }, 1000);
 
       let checkIfPlaying = true;
-      const playStatus = document.querySelector('#play-status');
-      playStatus.addEventListener('click', (e) => {
+      const playStatus = document.querySelector("#play-status");
+
+      playStatus.innerHTML = `
+    <i id='pause' class="fa-solid fa-pause fa-2x"></i>
+          `;
+      checkIfPlaying = false;
+      playStatus.addEventListener("click", (e) => {
         if (checkIfPlaying) {
           playStatus.innerHTML = `
-    <i id='pause' class="fa-solid fa-play fa-2x"></i>
+    <i id='pause' class="fa-solid fa-pause fa-2x"></i>
           `;
           checkIfPlaying = false;
           tts.resume();
-          tts('');
         } else {
           playStatus.innerHTML = `
 
-        <i id='play' class="fa-solid fa-pause fa-2x "></i>
+        <i id='play' class="fa-solid fa-play fa-2x "></i>
           `;
           checkIfPlaying = true;
           tts.pause();
@@ -118,10 +122,10 @@ const pdfViewer = (() => {
 
       // Move pages
 
-      document.addEventListener('keypress', (e) => {
-        if (e.key.toLowerCase() === 'p') {
+      document.addEventListener("keypress", (e) => {
+        if (e.key.toLowerCase() === "p") {
           showPreviousPage();
-        } else if (e.key.toLowerCase() === 'n') {
+        } else if (e.key.toLowerCase() === "n") {
           showNextPage();
         }
         e.preventDefault();
