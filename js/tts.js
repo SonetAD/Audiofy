7416547ac558a2cc07d1946e63c798be2c57a997
Sonet Adhikary <sonet.ad101@gmail.com>
2023-04-08 22:11:42 +0600

    now user can change voice,rate and pitch from ui

--- js/tts.js
+++ js/tts.js
@@ -1,5 +1,7 @@
 class TTS {
   static selectedVoice = JSON.parse(localStorage.getItem("selectedVoice"));
+  static rate = JSON.parse(localStorage.getItem("rate"));
+  static pitch = JSON.parse(localStorage.getItem("pitch"));
   constructor(text) {
     this.utter = new SpeechSynthesisUtterance();
     this.speakText = new SpeechSynthesisUtterance(text);
@@ -38,8 +40,8 @@ class TTS {
         this.speakText.voice = v;
       }
     });
-    this.speakText.rate = 1;
-    this.speakText.pitch = 1;
+    this.speakText.rate = TTS.rate;
+    this.speakText.pitch = TTS.pitch;
     speechSynthesis.speak(this.speakText);
   }
 
@@ -65,7 +67,11 @@ if (!voices) {
 }
 if (!TTS.selectedVoice) {
   localStorage.setItem("selectedVoice", voiceJsonConverter(voices[0]));
+  localStorage.setItem("rate", JSON.stringify(1));
+  localStorage.setItem("pitch", JSON.stringify(1));
   TTS.selectedVoice = voices[0];
+  TTS.pitch = 1;
+  TTS.rate = 1;
 }
 
 function voiceJsonConverter(voice) {
@@ -73,10 +79,11 @@ function voiceJsonConverter(voice) {
   for (v in voice) {
     modVoice[v] = voice[v];
   }
-  console.log(JSON.stringify(modVoice));
   return JSON.stringify(modVoice);
 }
 
+const chosenRate = document.getElementById("rate");
+const chosenPitch = document.getElementById("pitch");
 const changeVoiceBtn = document.getElementById("changeVoiceBtn");
 changeVoiceBtn.addEventListener("click", (e) => {
   const voiceSelectedBox = document.querySelector("#voice-select");
@@ -96,9 +103,29 @@ changeVoiceBtn.addEventListener("click", (e) => {
           localStorage.setItem("selectedVoice", voiceJsonConverter(v));
         }
       });
+      const rateVal = document.getElementById("rate-value");
+      chosenRate.addEventListener("change", (e) => {
+        rateVal.innerText = chosenRate.value;
+        console.log("rate change");
+        e.preventDefaulte();
+      });
+      localStorage.setItem("rate", JSON.stringify(chosenRate.value));
+      localStorage.setItem("pitch", JSON.stringify(chosenPitch.value));
       location.href = "index.html";
     }
     e.preventDefault();
   });
+  e.preventDefault();
+});
+
+const rateVal = document.getElementById("rate-value");
+chosenRate.addEventListener("change", (e) => {
+  rateVal.innerText = chosenRate.value;
+  e.preventDefault();
+});
+
+const pitchVal = document.getElementById("pitch-value");
+chosenPitch.addEventListener("change", (e) => {
+  pitchVal.innerText = chosenPitch.value;
   e.preventDefault();
 });
